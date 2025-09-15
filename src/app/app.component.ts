fc0bdbd85a9291c335a0c5ae5d4d5678f06dd43e
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 01:39:18 +0200

    start building website structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './features/main/main.component';
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    CommonModule,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent,
-  ],
-  template: `
-    <div class="min-h-screen bg-white">
-      <app-header></app-header>
-      <app-main></app-main>
-      <app-footer></app-footer>
-    </div>
-  `,
-  styles: []
+  imports: [CommonModule, RouterModule],
+  templateUrl: "./app.component.html",
+  styles: [],
 })
-export class AppComponent {
-}
+export class AppComponent {}
