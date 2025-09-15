094ac1408f8712e88bab6dc57c6756b7f6945ea1
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 01:39:18 +0200

    start building website structure

--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeroComponent } from './hero/hero.component';
-import { FeaturesComponent } from './features/features.component';
-import { ExamplesComponent } from './examples/examples.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeroComponent } from "./hero/hero.component";
+import { FeaturesComponent } from "./features/features.component";
+import { ExamplesComponent } from "./examples/examples.component";
+import { SidebarComponent } from "src/app/components/sidebar/sidebar.component";
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { RouterModule } from "@angular/router";
+import { FooterComponent } from "src/app/components/footer/footer.component";
 
 @Component({
-  selector: 'app-main',
+  selector: "app-main",
   standalone: true,
-  imports: [CommonModule, HeroComponent, FeaturesComponent, ExamplesComponent],
-  template: `
-    <main>
-      <app-hero></app-hero>
-      <app-features></app-features>
-      <app-examples></app-examples>
-    </main>
-  `,
-  styles: []
+  imports: [
+    CommonModule,
+    HeroComponent,
+    FeaturesComponent,
+    ExamplesComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RouterModule,
+    FooterComponent,
+  ],
+  templateUrl: "./main.component.html",
+  styles: [],
 })
-export class MainComponent {
-}
+export class MainComponent {}
