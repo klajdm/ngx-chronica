7692d6ef2e8ef86b8fc63d063f8afe18ba73019b
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 17:36:05 +0200

    update introduction view

--- src/app/features/main/introduction/introduction.component.ts
+++ src/app/features/main/introduction/introduction.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChronicaInlineCalendarComponent } from '../../../../../projects/chronica/src/public-api';
 
 @Component({
   selector: 'app-introduction',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, ChronicaInlineCalendarComponent],
   templateUrl: './introduction.component.html',
   styleUrls: ['./introduction.component.css'],
 })
-export class IntroductionComponent {}
+export class IntroductionComponent {
+  previewSelectedDate: Date | null = null;
+
+  onPreviewDateSelected(date: Date): void {
+    this.previewSelectedDate = date;
+    console.log('Intro preview date selected:', date);
+  }
+}
