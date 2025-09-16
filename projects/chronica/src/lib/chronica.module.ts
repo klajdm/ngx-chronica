186aec446871f4379625c2ed04d425e6665803e9
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 22:13:18 +0200

    implement a basic date-range component

--- projects/chronica/src/lib/chronica.module.ts
+++ projects/chronica/src/lib/chronica.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChronicaDatepickerComponent } from './components/datepicker/datepicker.component';
 import { ChronicaInlineCalendarComponent } from './components/inline-calendar/inline-calendar.component';
+import { ChronicaDateRangeComponent } from './components/date-range/date-range.component';
 
 @NgModule({
-  imports: [CommonModule, ChronicaDatepickerComponent, ChronicaInlineCalendarComponent],
-  exports: [ChronicaDatepickerComponent, ChronicaInlineCalendarComponent],
+  imports: [CommonModule, ChronicaDatepickerComponent, ChronicaInlineCalendarComponent, ChronicaDateRangeComponent],
+  exports: [ChronicaDatepickerComponent, ChronicaInlineCalendarComponent, ChronicaDateRangeComponent],
 })
 export class ChronicaModule {}
