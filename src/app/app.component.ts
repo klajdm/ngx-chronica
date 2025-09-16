1a2103e37a763c67734af4c74a69c08ffdc39a85
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 13:53:29 +0200

    general fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: "./app.component.html",
-  styles: [],
 })
 export class AppComponent {}
