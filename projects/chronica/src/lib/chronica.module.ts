75a7e868a3f6fdb837bf652b66cb35021fe7c506
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 17:40:41 +0200

    format project files with  prettier

--- projects/chronica/src/lib/chronica.module.ts
+++ projects/chronica/src/lib/chronica.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { ChronicaDatepickerComponent } from "./components/datepicker/datepicker.component";
-import { ChronicaInlineCalendarComponent } from "./components/inline-calendar/inline-calendar.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChronicaDatepickerComponent } from './components/datepicker/datepicker.component';
+import { ChronicaInlineCalendarComponent } from './components/inline-calendar/inline-calendar.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ChronicaDatepickerComponent,
-    ChronicaInlineCalendarComponent,
-  ],
+  imports: [CommonModule, ChronicaDatepickerComponent, ChronicaInlineCalendarComponent],
   exports: [ChronicaDatepickerComponent, ChronicaInlineCalendarComponent],
 })
 export class ChronicaModule {}
