64a394acba9f66dfcc1a7603637f856c3dcb046e
Klajdi Murataj <klajdimurati3@gmail.com>
2025-09-16 17:35:50 +0200

    update getting started

--- src/app/features/main/getting-started/getting-started.component.ts
+++ src/app/features/main/getting-started/getting-started.component.ts
@@ -1,10 +1,24 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ChronicaDatepickerComponent } from "../../../../../projects/chronica/src/lib/components/datepicker/datepicker.component";
+import { CalendarConfig, DEFAULT_CALENDAR_CONFIG } from "../../../../../projects/chronica/src/lib/models/chronica.models";
 
 @Component({
   selector: "app-getting-started",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ChronicaDatepickerComponent],
   templateUrl: "./getting-started.component.html",
 })
-export class GettingStartedComponent {}
+export class GettingStartedComponent {
+  // Example datepicker
+  exampleSelectedDate: Date | null = new Date(2025, 8, 15); // September 15, 2025
+  exampleConfig: CalendarConfig = {
+    ...DEFAULT_CALENDAR_CONFIG,
+    theme: 'light',
+    colorTheme: 'blue'
+  };
+
+  onExampleDateSelected(date: Date): void {
+    this.exampleSelectedDate = date;
+  }
+}
