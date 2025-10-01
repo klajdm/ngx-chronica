bbe94aba4d7a2d90a0d513b571534137059730ff
Klajdi Murataj <klajdimurati3@gmail.com>
2025-10-01 17:41:06 +0200

    add documentation page & styling guide page boilerplate

---
+++ src/app/features/main/features/documentation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentation',
+  imports: [],
+  templateUrl: './documentation.component.html',
+  styleUrl: './documentation.component.css',
+})
+export class DocumentationComponent {}
