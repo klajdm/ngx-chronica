d5e35074f71d3d3c9b985b1fda73149f062123d3
Klajdi Murataj <klajdimurati3@gmail.com>
2025-10-01 17:39:31 +0200

    add boilerplate for  COC

---
+++ src/app/features/main/code-of-conduct/code-of-conduct.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-of-conduct',
+  imports: [],
+  templateUrl: './code-of-conduct.component.html',
+  styleUrl: './code-of-conduct.component.css',
+})
+export class CodeOfConductComponent {}
