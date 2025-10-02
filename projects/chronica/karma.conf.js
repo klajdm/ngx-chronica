5c294a49249ddd54b0bd68991e48da398f4ce181
Klajdi Murataj <klajdimurati3@gmail.com>
2025-10-02 23:11:07 +0200

    update project configuration and package metadata for ngx-chronica library

--- projects/chronica/karma.conf.js
+++ projects/chronica/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/nga-inline-calendar'),
+      dir: require('path').join(__dirname, '../../coverage/ngx-chronica'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
