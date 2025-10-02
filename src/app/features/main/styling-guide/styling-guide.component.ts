7195dad8ae0684bf8322a95eee2a3f09172d37bb
Klajdi Murataj <klajdimurati3@gmail.com>
2025-10-02 13:33:33 +0200

    standardize component names and update styling guide routes

---
+++ src/app/features/main/styling-guide/styling-guide.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styling-guide',
+  imports: [],
+  templateUrl: './styling-guide.component.html',
+  styleUrl: './styling-guide.component.css',
+})
+export class StylingGuideComponent {}
