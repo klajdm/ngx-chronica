5e3ddba6e17ffae2cb6141d5a88f61f7900ceb31
Klajdi Murataj <klajdimurati3@gmail.com>
2025-10-04 00:58:12 +0200

    add config binding to chronica-inline-calendar in preview section

--- src/app/features/main/introduction/introduction.component.ts
+++ src/app/features/main/introduction/introduction.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChronicaInlineCalendarComponent } from '../../../../../projects/chronica/src/public-api';
+import { ThemeService } from '../../../services/theme.service';
+import { ChronicaCalendarConfig, DEFAULT_CALENDAR_CONFIG } from '../../../../../dist/chronica';
 
 @Component({
   selector: 'app-introduction',
@@ -10,7 +12,16 @@ import { ChronicaInlineCalendarComponent } from '../../../../../projects/chronic
   styleUrls: ['./introduction.component.css'],
 })
 export class IntroductionComponent {
+  private readonly _themeService = inject(ThemeService);
+
   previewSelectedDate: Date | null = null;
+  previewBasicConfig = computed(
+    (): ChronicaCalendarConfig => ({
+      ...DEFAULT_CALENDAR_CONFIG,
+      theme: 'light',
+      colorTheme: this._themeService.currentTheme(),
+    })
+  );
 
   onPreviewDateSelected(date: Date): void {
     this.previewSelectedDate = date;
